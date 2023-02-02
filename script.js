0f6867dbba2481b6320572ecd3f0ffb522ae1f7d
Amalie Forum <amalieforum@live.dk>
2023-02-02 11:18:02 +0100

    done

---
+++ script.js
@@ -0,0 +1,14 @@
+const superhelt = [
+  {
+    navn: "Superwoman",
+    køn: "Kvinde",
+    tøj: "Kjole",
+    egenskaber: ["flyve", "styrke", "usårlighed"],
+  },
+  {
+    navn: "Superman",
+    køn: "Mand",
+    tøj: "Dragt",
+    egenskaber: ["kræfter", "styrke", "flyve", "varmeblik"],
+  },
+];
